6491cdedd7f349661fd15e485afa1f99e21b22bb
Sudharshan Gokul Rajan <sudharshangokulrajan@gmail.com>
2021-11-06 22:20:26 +0530

    merged area of circle task and extension task in one file

--- area_of_circleAndextension.py
+++ area_of_circleAndextension.py
@@ -1,3 +1,11 @@
+#Task 1
+import math
+radius = float(input("Input the radius of the circle: "))
+area = math.pi * radius ** 2 
+print("The area of the circle with radius {} is {}".format(radius, area))
+
+
+#Task 2
 file_name = input("Input the Filename: ")
 my_Dict = {".py":"Python",".cpp":"C++", ".C":"C", ".java":"Java"}
 for i in range(0,len(file_name)):
